43d51e6e5e0b8bffbad5d3c4101e58698777e18c
Francisco Raigoza <raigoza.frankie@gmail.com>
2021-01-01 09:11:22 -0800

    created Sidebar component and starting on Feed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,43 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './Sidebar.css'
 
 const Sidebar = () => {
+
+
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
     return (
         <div className='sidebar'>
-            
+            <div className="sidebar__top">
+                <img src="https://images.pexels.com/photos/3394939/pexels-photo-3394939.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+                <Avatar className='sidebar__avatar'/>
+                <h2>Francisco Raigoza</h2>
+                <h4>frankieraigoza@gmail.com</h4>
+            </div>
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                  <p>Who viewed you</p>  
+                  <p className='sidebar__statNumber'>2.543</p>  
+                </div>
+                <div className="sidebar__stat">
+                  <p>Views on post</p>  
+                  <p className='sidebar__statNumber'>2.454</p>
+                </div>
+            </div>
+            <div className="sidebar__bottom">
+                <p>Recent</p>
+                {recentItem('reactjs')}
+                {recentItem('programming')}
+                {recentItem('softwareengineering')}
+                {recentItem('design')}
+                {recentItem('developer')}
+            </div>
         </div>
     )
 }
